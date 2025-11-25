2c0659b65133c51d5f38f78a35718a0bf8b33248
Edgar Franco <edfsosa@outlook.com>
2025-11-25 12:41:33 -0300

    Mejoras en la vista routines.show

--- app/Models/RoutineExercise.php
+++ app/Models/RoutineExercise.php
@@ -50,4 +50,38 @@ class RoutineExercise extends Model
     {
         return $this->exercise ? $this->exercise->description : null;
     }
+
+    /**
+     * Obtiene la duración en segundos o minutos formateada según el valor.
+     */
+    public function getDurationAttribute()
+    {
+        if ($this->duration_seconds >= 60) {
+            $minutes = floor($this->duration_seconds / 60);
+            return "{$minutes} min";
+        } else {
+            return "{$this->duration_seconds} sec";
+        }
+    }
+
+    /**
+     * Obtiene el descanso en segundos o minutos formateada según el valor.
+     */
+    public function getRestAttribute()
+    {
+        if ($this->rest_seconds >= 60) {
+            $minutes = floor($this->rest_seconds / 60);
+            return "{$minutes} min";
+        } else {
+            return "{$this->rest_seconds} sec";
+        }
+    }
+
+    /**
+     * Obtiene el peso en kg formateado como entero sin decimales.
+     */
+    public function getWeightKgFormattedAttribute()
+    {
+        return $this->weight_kg ? number_format($this->weight_kg, 0) . ' kg' : null;
+    }
 }
